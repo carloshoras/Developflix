61cf2f371462ad0ed7a6fcd739dd0f2b0e26a4ee
carloshoras <carloshoras>
2023-11-13 04:06:07 +0100

    Falta maquetar

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 import peliculas from './peliculas.js'
 
+const divGeneros = document.querySelectorAll('.genero')
+const generos = [28, 53, 12]
+
+function postMovie (genre, element) {
+    if (genre == 28 || genre == 53 || genre == 12) {
+        const pelicula = document.createElement('div')
+        pelicula.setAttribute('class', 'poster')
+        pelicula.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${element.poster_path}" alt="${element.title}" /><span>${element.title}</span>`
+        divGeneros[generos.indexOf(genre)].appendChild(pelicula)
+    }
+}
+
+peliculas.forEach((element) => element.genre_ids.forEach((genre) => postMovie(genre, element)))
